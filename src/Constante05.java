8bb31af7d320b5709e33b2d8cbbe53c8e1d547f9
DOMIGHER <Domi_ig2000@hotmail.com>
2025-03-31 21:41:42 -0600

    Curso basico de programacion java - 31-03

---
+++ src/Constante05.java
@@ -0,0 +1,12 @@
+public class Constante05 {
+
+    public static void main(String[] args) {
+        //La constante es un espacio en memoria donde su valor no cambia nunca
+
+        final int numero1 = 12;
+
+        System.out.println("La constante es " + numero1);
+
+
+    }
+}
